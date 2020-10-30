54284930e3220496b01c636b99c2f3e46d28bad2
Rory Hart <hartror@gmail.com>
2020-10-30 21:27:29 +1100

    Finished raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut ret = String::from("");
+    if n % 3 == 0 {
+        ret.push_str("Pling")
+    }
+    if n % 5 == 0 {
+        ret.push_str("Plang")
+    }
+    if n % 7 == 0 {
+        ret.push_str("Plong")
+    }
+    if ret.len() == 0 {
+        ret = n.to_string()
+    }
+    ret
 }
