a0f8ac05ecbc126fb548e24510445cc915f01bf7
Rory Hart <hartror@gmail.com>
2020-10-30 21:36:15 +1100

    Improved raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,16 +1,16 @@
 pub fn raindrops(n: u32) -> String {
     let mut ret = String::from("");
     if n % 3 == 0 {
-        ret.push_str("Pling")
+        ret.push_str("Pling");
     }
     if n % 5 == 0 {
-        ret.push_str("Plang")
+        ret.push_str("Plang");
     }
     if n % 7 == 0 {
-        ret.push_str("Plong")
+        ret.push_str("Plong");
     }
-    if ret.len() == 0 {
-        ret = n.to_string()
+    if ret.is_empty() {
+        ret = n.to_string();
     }
     ret
 }
