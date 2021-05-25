5671c138b2dd87854a89c18d006112963567882c
Rory Hart <rory.hart@airwallex.com>
2021-05-25 22:12:13 +1000

    diamond

--- kotlin/diamond/src/main/kotlin/Diamond.kt
+++ kotlin/diamond/src/main/kotlin/Diamond.kt
@@ -1,15 +1,14 @@
 class DiamondPrinter {
 
     fun printToList(c: Char) : List<String> {
-        return listOf(('A'..c).toList(), (c - 1 downTo 'A').toList())
-            .flatten()
+        return (('A'..c) + (c - 1 downTo 'A'))
             .map {
                 when (it) {
-                    'A' -> " ".repeat(c - 'A') + "A" + " ".repeat(c - 'A')
+                    'A' -> "${" ".repeat(c - 'A')}A${" ".repeat(c - 'A')}"
                     else -> " ".repeat(c - it) +
-                            it.toString() +
-                            " ".repeat( (c - 'A')*2 - (c - it)*2 - 1) +
-                            it.toString() +
+                            it +
+                            " ".repeat( (c - 'A') * 2 - (c - it) * 2 - 1) +
+                            it +
                             " ".repeat(c - it)
                 }
             }
