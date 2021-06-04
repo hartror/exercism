68dc5597e0fd42580b280aeb1287808af0e9f354
Rory Hart <rory.hart@airwallex.com>
2021-06-05 09:06:32 +1000

    Cleaner Gigasecond

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,14 +1,11 @@
-import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 
-private object GigasecondConst {
-    const val GS_SECONDS: Long = 1000000000 // 10^9 seconds
-}
+const val GS_SECONDS: Long = 1000000000 // 10^9 seconds
 
 class Gigasecond(inDateTime: LocalDateTime) {
-    val date: LocalDateTime = inDateTime.plusSeconds(GigasecondConst.GS_SECONDS)
+    val date: LocalDateTime = inDateTime.plusSeconds(GS_SECONDS)
 
     constructor(inDate: LocalDate) : this(inDate.atStartOfDay())
 }
